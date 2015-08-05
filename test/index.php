1541b021746065ba891067fa5e7596b4da3b64fd
ALexander Balance <appproduction@yandex.ru>
2015-08-05 17:22:12 +0300

    Тестовый файл php

---
+++ test/index.php
@@ -0,0 +1,36 @@
+<?php
+$a = 'Вася Пупкин';
+$b = 7;
+define('WEEK', seven_days)
+?>
+<html>
+<head>
+    <title></title>
+
+</head>
+<body>
+<h1>Заголовок</h1>
+<ul>
+   <li>
+       <?php
+   echo $a + $b;
+   ?>
+   </li>
+    <li>
+        <?php
+    echo seven_days;
+    ?>
+    </li>
+<li>
+   <?php
+   echo 'seven_days';
+   ?>
+</li>
+</ul>
+
+</body>
+
+</html>
+<?php
+
+?>
